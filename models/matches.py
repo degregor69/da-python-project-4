e22323fd5a5822ede188e1627e5255269774307b
degregor69 <raphael.degregorio@gmail.com>
2024-11-08 10:00:54 +0100

    updated player scores in matches from int to float

--- models/matches.py
+++ models/matches.py
@@ -2,7 +2,7 @@ from models.players import Player
 
 
 class Match:
-    def __init__(self, player_1: Player, score_player_1: int,player_2: Player, score_player_2):
+    def __init__(self, player_1: Player, score_player_1: float,player_2: Player, score_player_2: float):
         self.player_1 = player_1
         self.score_player_1 = score_player_1
         self.player_2 = player_2
