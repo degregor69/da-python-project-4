ffce975dd41dc021899a8b186ff3e94a110e9fcd
degregor69 <raphael.degregorio@gmail.com>
2024-11-08 10:23:24 +0100

    corrected models to be able to get unique players, types in players

--- models/players.py
+++ models/players.py
@@ -1,5 +1,8 @@
+import datetime
+
+
 class Player:
-    def __init__(self, last_name: str, first_name: str, birth_date: str, national_id: str):
+    def __init__(self, last_name: str, first_name: str, birth_date: datetime.date, national_id: str):
         self.last_name = last_name
         self.first_name = first_name
         self.birth_date = birth_date
--- models/rounds.py
+++ models/rounds.py
@@ -1,11 +1,14 @@
 import datetime
+from typing import List
 
 from models.matches import Match
 
 
+
 class Round:
-    def __init__(self, name: str, start_datetime: datetime.datetime, end_datetime: datetime.datetime, matches: [Match]):
+    def __init__(self, name: str, start_datetime: datetime.datetime, end_datetime: datetime.datetime, matches: List[Match]):
         self.name = name
         self.start_datetime = start_datetime
         self.end_datetime = end_datetime
         self.matches = matches
+
