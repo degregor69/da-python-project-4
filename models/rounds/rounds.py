586c2c3fe23913cefafb6920e53623220da5c0ed
degregor69 <raphael.degregorio@gmail.com>
2024-11-22 08:54:13 +0100

    added id counter, id, to_dict and from_dict function

--- models/rounds/rounds.py
+++ models/rounds/rounds.py
@@ -1,14 +1,37 @@
 import datetime
 from typing import List
 
-from models.matches.matches import Match
-
-
-
 class Round:
-    def __init__(self, name: str, start_datetime: datetime.datetime, end_datetime: datetime.datetime, matches: List[Match]):
+    _id_counter = 1  # Variable de classe pour suivre le prochain ID
+
+    def __init__(
+        self,
+        name: str,
+        start_datetime: datetime.datetime,
+        end_datetime: datetime.datetime,
+        matches: List[int],  # Liste d'IDs (int) des matchs
+    ):
+        self.id = Round._id_counter  # Attribuer un ID unique au round
+        Round._id_counter += 1  # Incrémenter le compteur pour le prochain round
         self.name = name
         self.start_datetime = start_datetime
         self.end_datetime = end_datetime
         self.matches = matches
 
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "start_datetime": self.start_datetime.isoformat(),
+            "end_datetime": self.end_datetime.isoformat(),
+            "matches": self.matches,
+        }
+
+    @classmethod
+    def from_dict(cls, data: dict):
+        return cls(
+            name=data["name"],
+            start_datetime=datetime.datetime.fromisoformat(data["start_datetime"]),
+            end_datetime=datetime.datetime.fromisoformat(data["end_datetime"]),
+            matches=data["matches"],
+        )
