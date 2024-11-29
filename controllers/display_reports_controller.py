89e6ebc9d7cb0b90915872da17fe43f94547f5d1
degregor69 <raphael.degregorio@gmail.com>
2024-11-29 12:22:02 +0100

    added display of all tournament information when selected

--- controllers/display_reports_controller.py
+++ controllers/display_reports_controller.py
@@ -1,18 +1,26 @@
 from models.players.players_manager import PlayersManager
+from models.tournaments.tournaments_manager import TournamentsManager
 from views.reports import ReportsViews
+from views.tournaments import StartTournamentViews
 
 
 class DisplayReportsController:
     def __init__(self):
         self.players_manager = PlayersManager()
+        self.tournaments_manager = TournamentsManager()
 
     def run(self):
         selection = ReportsViews.reports_selection()
-        if selection == 1: # Display all player by alphabetical order
+        if selection == 1: # Display all players by alphabetical order
             self.display_players_by_alphabetical_order()
+        if selection == 2: # Display all tournaments
+            all_tournaments = self.tournaments_manager.get_all_tournaments()
+            tournament = StartTournamentViews.select_tournament(all_tournaments)
+            ReportsViews.display_tournament_details(tournament)
 
 
     def display_players_by_alphabetical_order(self):
         players = self.players_manager.get_all_players()
         sorted_players = sorted(players, key=lambda player: player.full_name)
         ReportsViews.display_players(sorted_players)
+
--- views/reports.py
+++ views/reports.py
@@ -1,4 +1,5 @@
 from models.players.players import Player
+from models.tournaments.tournaments import Tournament
 
 
 class ReportsViews:
@@ -17,3 +18,19 @@ class ReportsViews:
     def display_players(players: list[Player]):
         for player in players:
             print(f"Nom : {player.full_name} | Date de naissance : {player.birth_date} | Points : {player.points}")
+
+    @staticmethod
+    def display_tournament_details(tournament: Tournament):
+        print("Voici les détails du tournoi :")
+        print(f"Nom : {tournament.name}")
+        print(f"Date de début : {tournament.start_date} | Date de fin : {tournament.end_date}")
+        print("\n Liste des joueurs par ordre alphabétique :")
+        sorted_players = sorted(tournament.players, key=lambda player: player.full_name)
+        ReportsViews.display_players(sorted_players)
+        print("\n Liste des tours :")
+        for round in tournament.rounds:
+            print(f"Tour : {round.name}")
+            print(f"Date de début : {round.start_datetime} | Date de fin : {round.end_datetime}")
+            print("\nMatchs du round : ")
+            for match in round.matches:
+                print(f"{match.player_1.national_id} | {match.score_player_1} vs {match.player_2.national_id} | {match.score_player_2}")
